0fbb5b2dbd025ab7fcde7594735eaf2a6d916d6e
Alex Theodoridis <alekstheod@gmail.com>
2023-12-10 12:50:06 +0100

    Adjust basic keymappings and install neotree

---
+++ lua/commands.lua
@@ -0,0 +1,3 @@
+-- Custom commands
+
+vim.api.nvim_create_user_command('Q', 'qa!', {})
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,12 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- windows navigation
+vim.keymap.set('n', '<C-j>', '<C-w>j', { silent = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { silent = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { silent = true })
+vim.keymap.set('n', '<C-h>', '<C-w>h', { silent = true })
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
@@ -13,6 +19,9 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Neotree
+vim.keymap.set('n', '<C-space>', '<Cmd>Neotree toggle<CR>')
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
