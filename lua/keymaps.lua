35833c6c5e3bc5bd18d21d791aabe94af9bd5f65
Alexander Bergeron <alexander.bergeron@protonmail.com>
2024-01-27 20:16:42 -0600

    add keymap to exit back

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,6 +4,9 @@
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
+-- Add remap to exit back to navigator
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
