1ea504478bf08aa8a659ea27c40d186a7de14957
James Krueger <jdkrueger12@gmail.com>
2024-03-20 08:02:39 -0400

    tries adding harpoon config, fails to load

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -4,4 +4,8 @@ return {
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
   },
+  config = function()
+    local harpoon = require 'harpoon'
+    harpoon:setup()
+  end,
 }
