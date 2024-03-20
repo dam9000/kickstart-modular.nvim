8d1455fc866c526b2063b25902a6fe985cbbd822
lauwnch <jdkrueger12@gmail.com>
2024-03-20 14:39:24 -0400

    fixes harpoon config and setup

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -3,9 +3,9 @@ return {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local harpoon = require 'harpoon'
+      harpoon:setup()
+    end,
   },
-  config = function()
-    local harpoon = require 'harpoon'
-    harpoon:setup()
-  end,
 }
