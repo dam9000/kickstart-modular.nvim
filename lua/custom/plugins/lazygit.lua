b7e4c5f17586c73777035b490bc0af15908bc3c5
James Krueger <jdkrueger12@gmail.com>
2024-03-21 21:05:46 -0400

    add rustacean, lazygit, and keys

---
+++ lua/custom/plugins/lazygit.lua
@@ -0,0 +1,21 @@
+return {
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+  keys = {
+    {
+      '<leader>gg',
+      '<cmd>LazyGit<cr>',
+      desc = 'Open LazyGit',
+    },
+  },
+}
