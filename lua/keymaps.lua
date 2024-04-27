561f7d5ce6790172d8ba51c01febe1d34e72a29c
kikesensei <enrique.delgado@hellofresh.com>
2024-04-27 11:30:40 +0200

    Update keymaps and comments

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -60,15 +60,20 @@ vim.keymap.set('n', '<C-S-h>', ':vertical resize -2<CR>', { silent = true, desc 
 vim.keymap.set('n', '<C-S-l>', ':vertical resize +2<CR>', { silent = true, desc = 'Increase window size horizontally' })
 
 -- Mappings to manage buffers
-vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Go to [N]ext [B]uffer' })
-vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = 'Go to [P]revious [B]uffer' })
-vim.keymap.set('n', '<leader>bd', ':bdelete<CR>', { desc = '[D]elete current [B]uffer' })
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Goto [N]ext [B]uffer' })
+vim.keymap.set('n', '<leader>bp', ':bprevious<CR>', { desc = 'Goto [P]revious [B]uffer' })
+vim.keymap.set('n', '<leader>bd', ':bd|edit#<CR>', { desc = '[D]elete current [B]uffer' })
+vim.keymap.set('n', '<leader>ba', ':%bd|edit#|bd#<CR>', { desc = 'Delete [A]ll [B]uffers except current' })
 
 -- Mappings to manage windows
 vim.keymap.set('n', '<leader>wh', '<C-W>s', { desc = 'Split [W]indow [H]orizontally' })
 vim.keymap.set('n', '<leader>wv', '<C-W>v', { desc = 'Split [W]indow [V]ertically' })
 vim.keymap.set('n', '<leader>we', '<C-W>=', { desc = 'Make split [W]indows [E]qual width' })
 vim.keymap.set('n', '<leader>wq', '<C-W>q', { desc = '[Q]uit current [W]indow' })
+
+-- Copilot keymaps
+vim.keymap.set('i', '<S-l>', '<Plug>(copilot-accept-word)', { desc = 'Accept Copilot next word suggestion' })
+vim.keymap.set('i', '<C-S-l>', '<Plug>(copilot-accept-line)', { desc = 'Accept Copilot next line suggestion' })
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
