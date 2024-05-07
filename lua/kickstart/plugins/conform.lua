f12d475d15e4714c8b19facef67621703e440495
kikesensei <enrique.delgado@hellofresh.com>
2024-05-07 12:22:20 +0200

    Add linter plugin, add keymaps for gitsigns

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,14 +1,29 @@
--- [[ Configure Autoformatting ]]
+-- [[ Configure Formatters and Autoformatting ]]
 
 return {
   {
     'stevearc/conform.nvim',
+    lazy = false,
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = 'n',
+        desc = '[F]ormat the current buffer',
+      },
+    },
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 5000,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable `format_on_save lsp_fallback` for languages that don't have a well standardized coding style.
+        local disabled_fts = { c = true, cpp = true, objc = true, objcpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disabled_fts[vim.bo[bufnr].filetype],
+        }
+      end,
       formatters_by_ft = {
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -67,8 +67,8 @@ return {
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = 'Search Open Files' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
@@ -78,7 +78,7 @@ return {
           previewer = false,
           prompt_title = 'Live grep in current buffer',
         })
-      end, { desc = 'Search in current buffer' })
+      end, { desc = 'Search by Grep in current buffer' })
 
       -- Also possible to pass additional configuration options.
       --  See `:help telescope.builtin.live_grep()` for information about particular keys
