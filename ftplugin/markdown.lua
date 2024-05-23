ad72418f3388f835c9bb209b516f4e28a40eb36e
kikesensei <enrique.delgado@hellofresh.com>
2024-05-23 10:28:53 +0200

    Add company greeter

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,3 +3,12 @@
 -- Spell check
 vim.opt.spelllang = 'en_us'
 vim.opt.spell = true
+
+-- Set configuration for tabs
+-- See `:help tabstop`
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.softtabstop = 2
+
+-- vim: ts=2 sts=2 sw=2 et
