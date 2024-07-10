70af1cc974011dfea1c5982c8aed3d7990eef8d5
Varun Rajput <varun@digitallyinduced.com>
2024-07-10 10:03:01 -0400

    set catppuccin default

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -2,4 +2,17 @@ return {
   'catppuccin/nvim',
   name = 'catppuccin',
   priority = 1000,
+  config = function()
+    require('catppuccin').setup {
+      flavour = 'macchiato', -- You can change this to "latte", "frappe", "macchiato", or "mocha"
+      background = { light = 'latte', dark = 'mocha' },
+      transparent_background = false,
+      term_colors = true,
+      integrations = {
+        nvimtree = true,
+        -- Add other integrations here
+      },
+    }
+    vim.cmd [[colorscheme catppuccin]]
+  end,
 }
