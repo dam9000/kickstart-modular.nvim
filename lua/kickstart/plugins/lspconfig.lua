ea7f856bdfe4d0f20a4b4161f76d1785fb57f1fa
SafetyMary <72537260+SafetyMary@users.noreply.github.com>
2024-08-03 01:29:09 +0800

    Initial settings

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -208,6 +208,8 @@ return {
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'pyright',
+        -- 'ruff', todo: need to fix auto format on write
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
