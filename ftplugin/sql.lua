a21f4a44720aed05bc3cd8a3c8b2aaaaaaaf1db5
kikesensei <enrique.delgado@hellofresh.com>
2024-08-25 14:15:41 +0200

    Remove formatting files

--- ftplugin/sql.lua
+++
@@ -1,8 +0,0 @@
--- Set configuration for tabs
--- See `:help tabstop`
-vim.opt.tabstop = 8
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = false
-vim.opt.softtabstop = 2
-
--- vim: ts=2 sts=2 sw=2 et
--- lua/options.lua
+++ lua/options.lua
@@ -14,7 +14,7 @@ vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
+--  See `:help clipboard`
 vim.opt.clipboard = 'unnamedplus'
 
 -- Keeps indentation when wrapping lines for not fitting on the window
@@ -43,8 +43,8 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 -- Sets how neovim will display certain whitespace in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
+--  See `:help list`
+--  and `:help listchars`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣', lead = '·' }
 
