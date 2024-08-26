98e08359f0949a951d79a2141bfbecc6dab5d884
Varun Rajput <rvarun011@gmail.com>
2024-08-25 23:39:39 -0400

    disable autofmts, add harpoon and mini-files

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -5,9 +5,10 @@ return {
     null_ls.setup {
       source = {
         null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.black,
+        -- null_ls.builtins.formatting.isort,
+        -- null_ls.builtins.formatting.black,
         null_ls.builtins.diagnostics.mypy,
+        -- null_ls.builtins.formatting.prettier,
       },
     }
   end,
