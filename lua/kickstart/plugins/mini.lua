27b2647fa0cf7fa87cb0fb7b09e14900eb2da14e
Varun Rajput <rvarun011@gmail.com>
2024-08-28 18:02:31 -0400

    add oil.nvim

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -32,7 +32,12 @@ return {
         return '%2l:%-2v'
       end
 
-      require('mini.files').setup()
+      -- local mini_files = require 'mini.files'
+      -- mini_files.setup()
+      --
+      -- vim.keymap.set('n', '<leader>mf', function()
+      --   mini_files.open()
+      -- end)
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
