66d639f1345d8140b1fc9b9d02a8f21550047379
Safety_Mary <72537260+SafetyMary@users.noreply.github.com>
2024-09-01 11:34:37 +0800

    Add treesitter-context

--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -39,7 +39,6 @@ return {
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
       incremental_selection = {
         enable = true,
         keymaps = {
@@ -49,6 +48,7 @@ return {
           node_decremental = 'grm',
         },
       },
+      indent = { enable = true, disable = { 'ruby' } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
