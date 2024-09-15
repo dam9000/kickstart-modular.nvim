8fadcc893cd52566b9be7eb5eed2bb95f1d0c2c9
pogacha <89231282@student.upr.si>
2024-09-15 23:14:05 +0200

    minor

--- lua/custom/plugins/nvim-r.lua
+++ lua/custom/plugins/nvim-r.lua
@@ -4,7 +4,7 @@ return {
     ft = { 'r', 'rmd' }, -- Load the plugin for R and R Markdown files
     config = function()
       vim.g.R_auto_start = 1
-      vim.g.R_hl_term = 0
+      vim.g.R_hl_term = 1
       vim.g.R_quiet = 1
       vim.g.R_assign = false
     end,
