58d3f8289178e42ba7c52edd66a58b9f3e9ada39
Abdul Aala Khan <abdulaala.khan@stud.h-da.de>
2024-11-24 15:01:36 +0100

    add todo binds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -59,4 +59,12 @@ vim.api.nvim_create_autocmd('TermOpen', {
     vim.cmd 'startinsert' -- Enter Insert mode
   end,
 })
+
+vim.keymap.set('n', ']t', function()
+  require('todo-comments').jump_next()
+end, { desc = 'Next todo comment' })
+
+vim.keymap.set('n', '[t', function()
+  require('todo-comments').jump_prev()
+end, { desc = 'Previous todo comment' })
 -- vim: ts=2 sts=2 sw=2 et
