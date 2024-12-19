2aa69562e8681adfd83a5c55f8d848ec30b2fffa
Aida (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2024-12-19 17:18:43 +0100

    Got debugger to work for rust~

--- lua/custom/plugins/rustaceanvim.lua
+++ lua/custom/plugins/rustaceanvim.lua
@@ -1,7 +1,13 @@
-return {
+return { -- Better tooling for rust than lspconfig built in.
+  -- References:
+  -- Configure Neovim For Easy Rust Development by Mental Outlaw https://www.youtube.com/watch?v=8nWzcVq5l3g&t=263s
+  -- Rust and Neovim are AMAZING together by typecraft https://www.youtube.com/watch?v=gihHLsClHF0
   {
     'mrcjkb/rustaceanvim',
     version = '^5', -- Recommended
-    lazy = false, -- This plugin is already lazy
+    lazy = false, -- This plugin is already lazy,
+    ['rust-analyzer'] = {
+      cargo = { allFeatures = true },
+    },
   },
 }
