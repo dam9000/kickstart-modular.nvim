587823c05e0a225061704343b7c581ebe07b518c
Aida (Laptop) <39270007+Aidavdw@users.noreply.github.com>
2024-12-21 17:33:33 +0100

    rustaceanvim keybinds

---
+++ after/ftplugin/rust.lua
@@ -0,0 +1,25 @@
+-- This file contains settings that are only applied to rust files.
+-- Keybinds must be set with `buffer = true`, if you don't want the bindings to be permanent for all buffers once it's been loaded.
+vim.keymap.set('n', '<leader>us', function()
+  vim.cmd.RustLsp { 'testables' }
+end, { desc = '[S]how testables ', buffer = true })
+
+vim.keymap.set('n', '<leader>ua', function()
+  vim.cmd.RustLsp { 'testables', bang = true }
+end, { desc = 'test previous [A]gain ', buffer = true })
+
+vim.keymap.set('n', '<leader>da', function()
+  vim.cmd.RustLsp { 'debuggables', bang = true }
+end, { desc = 'test previous [A]gain ', buffer = true })
+
+vim.keymap.set('n', '<leader>pe', function()
+  vim.cmd.RustLsp { 'explainError', 'current' }
+end, { desc = '[E]xplain this error ', buffer = true })
+
+vim.keymap.set('n', '<leader>pd', function()
+  vim.cmd.RustLsp { 'renderDiagnostic', 'current' }
+end, { desc = '[P]opup [D]iagnostic ', buffer = true })
+
+vim.keymap.set('n', 'gR', function()
+  vim.cmd.RustLsp { 'relatedDiagnostics' }
+end, { desc = '[G]oto [R]elated diagnostic ', buffer = true })
