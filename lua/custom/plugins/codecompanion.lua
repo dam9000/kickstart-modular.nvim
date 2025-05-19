48adbd9c2c49d13a7574571b259239ab7b92e03f
Jack Kelly <jack@openclimatefix.org>
2025-05-19 10:16:23 +0100

    Configure codecompanion to use Gemini

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -1,10 +1,39 @@
 return {
   {
     'olimorris/codecompanion.nvim',
-    opts = {},
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        ft = { 'codecompanion' },
+      },
+    },
+    opts = {
+      strategies = {
+        chat = {
+          adapter = 'gemini',
+        },
+        inline = {
+          adapter = 'gemini',
+        },
+      },
+      adapters = {
+        gemini = function()
+          return require('codecompanion.adapters').extend('gemini', {
+            schema = {
+              model = {
+                default = 'gemini-2.0-flash-lite',
+              },
+            },
+          })
+        end,
+        display = {
+          diff = {
+            provider = 'mini_diff',
+          },
+        },
+      },
     },
   },
 }
