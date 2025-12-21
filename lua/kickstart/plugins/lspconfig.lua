bd89dba078685e01c8e9e80b40ac1b091194098e
BasedScott <lotromen@gmail.com>
2025-12-21 11:32:02 -0800

    ok

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -210,8 +210,8 @@ return {
       local servers = {
 
         -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
+        gopls = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
